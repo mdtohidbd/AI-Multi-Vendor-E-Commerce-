ff866972c690021c542b684b55dd1d5218d18666
mdtohidvai <mdtohid242024@gmail.com>
2026-01-13 02:05:39 +0600

    first commit

--- inngest/client.js
+++ inngest/client.js
@@ -1,4 +1,4 @@
 import { Inngest } from "inngest";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "docart-ecommerce" });
+export const inngest = new Inngest({ id: "GoCart-ecommerce" });
